4b2721439241db40f45b65475d0c0e9e0430e654
sanchxt <tpbeast007@gmail.com>
2024-09-16 16:18:07 +0530

    solve valid parantheses problem

---
+++ leetcode/easy/Solution20.java
@@ -0,0 +1,25 @@
+package leetcode.easy;
+
+import java.util.Stack;
+
+public class Solution20 {
+  public boolean isValid(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (char c : s.toCharArray()) {
+      if (c == '(' || c == '{' || c == '[') {
+        stack.push(c);
+      } else {
+        if (stack.isEmpty()) {
+          return false;
+        }
+        char top = stack.peek();
+        if ((c == ')' && top == '(') || (c == '}' && top == '{') || (c == ']' && top == '[')) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+    return stack.isEmpty();
+  }
+}
