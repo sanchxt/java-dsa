4fc8ba51dbc63e1ed773aef414283fd8aa59e0d2
sanchxt <tpbeast007@gmail.com>
2024-09-16 15:51:56 +0530

    next greater element solution via stacks

---
+++ stacks/NextGreaterElement.java
@@ -0,0 +1,40 @@
+package stacks;
+
+import java.util.Stack;
+
+public class NextGreaterElement {
+  public static void displayArr(int[] arr) {
+    for (int i : arr) {
+      System.out.print(i + ", ");
+    }
+  }
+
+  public static int[] nextGreaterElement(int[] arr) {
+    int[] result = new int[arr.length];
+    Stack<Integer> stack = new Stack<>();
+    for (int i = arr.length - 1; i >= 0; i--) {
+      if (!stack.isEmpty()) {
+        while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+          stack.pop();
+        }
+      }
+
+      if (stack.isEmpty()) {
+        result[i] = -1;
+      } else {
+        result[i] = stack.peek();
+      }
+      stack.push(arr[i]);
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int arr[] = {4, 7, 3, 4, 8, 1};
+    int result[] = nextGreaterElement(arr);
+
+    displayArr(arr);
+    System.out.println("\n");
+    displayArr(result);
+  }
+}
