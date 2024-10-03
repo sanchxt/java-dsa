225b5ece6513ffac4019d5c973aa3d08b53e2461
sanchxt <tpbeast007@gmail.com>
2024-10-03 23:55:22 +0530

    solve pattern questions

---
+++ patterns/Pattern04.java
@@ -0,0 +1,16 @@
+package patterns;
+
+public class Pattern04 {
+  public static void pattern(int n) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j <= i; j++) {
+        System.err.print(i + 1);
+      }
+      System.err.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    pattern(5);
+  }
+}
