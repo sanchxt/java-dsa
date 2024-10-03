b2dbffb7c15fade5e39a555e812d13da6ea0e99b
sanchxt <tpbeast007@gmail.com>
2024-10-04 00:16:36 +0530

    solve flipped equilateral triangle pattern

---
+++ patterns/Pattern08.java
@@ -0,0 +1,24 @@
+package patterns;
+
+public class Pattern08 {
+  public static void pattern(int n) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j <= i; j++) {
+        System.err.print(" ");
+      }
+
+      for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+        System.err.print("* ");
+      }
+
+      for (int j = 0; j <= i; j++) {
+        System.err.print(" ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    pattern(5);
+  }
+}
