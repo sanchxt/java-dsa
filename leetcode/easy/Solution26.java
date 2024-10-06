121a175f863c337c18df4c947fd61a8e28129892
sanchxt <tpbeast007@gmail.com>
2024-10-06 20:24:05 +0530

    solve problem 26

---
+++ leetcode/easy/Solution26.java
@@ -0,0 +1,14 @@
+package leetcode.easy;
+
+public class Solution26 {
+  public int removeDuplicates(int[] nums) {
+    int i = 0;
+    for (int j = 1; j < nums.length; j++) {
+      if (nums[i] != nums[j]) {
+          i++;
+          nums[i] = nums[j];
+      }
+    }
+    return i + 1;
+  }
+}
