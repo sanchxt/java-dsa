e91688b605dea988275d1dc671dec9cf8de5e548
sanchxt <tpbeast007@gmail.com>
2024-10-06 20:24:30 +0530

    fix indentation

--- leetcode/easy/Solution1752.java
+++ leetcode/easy/Solution1752.java
@@ -4,7 +4,7 @@ public class Solution1752 {
   public boolean check(int[] nums) {
     int drop = 0;
     for (int i = 1; i < nums.length; i++) {
-        if (nums[i - 1] > nums[i]) drop++;
+      if (nums[i - 1] > nums[i]) drop++;
     }
     if (nums[0] < nums[nums.length - 1]) drop++;
     return drop <= 1;
