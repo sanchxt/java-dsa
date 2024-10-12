b848c5b338d01b8f1de5668d0fd51ce241976c86
sanchxt <tpbeast007@gmail.com>
2024-10-12 20:30:58 +0530

    solve problem 169

---
+++ leetcode/easy/Solution169.java
@@ -0,0 +1,12 @@
+package leetcode.easy;
+
+public class Solution169 {
+  public int majorityElement(int[] nums) {
+    int count = 0, candidate = 0;
+    for (int num : nums) {
+      if (count == 0) candidate = num;
+      count += (num == candidate) ? 1 : -1;
+    }
+    return candidate;
+  }
+}
