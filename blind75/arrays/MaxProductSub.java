7d9c445420b8e9e83ebcadc62dbffea2013edaa3
sanchxt <tpbeast007@gmail.com>
2024-10-16 03:00:39 +0530

    add optimal solution

--- blind75/arrays/MaxProductSub.java
+++ blind75/arrays/MaxProductSub.java
@@ -31,9 +31,37 @@ public class MaxProductSub {
     return result;
   }
 
+  // tc: O(N) , sc: O(1)
+  static int maxProductSubArrayOptimal(int arr[]) {
+    int n = arr.length;
+    int pre = 1, suff = 1;
+    int ans = Integer.MIN_VALUE;
+    for (int i = 0; i < n; i++) {
+      if (pre == 0) pre = 1;
+      if (suff == 0) suff = 1;
+      pre *= arr[i];
+      suff *= arr[n - i - 1];
+      ans = Math.max(ans, Math.max(pre, suff));
+    }
+    return ans;
+  }
+
+  // tc: O(N) , sc: O(1)
+  // AVOID TELLING THIS ALGO!
+  static int maxProductSubArrayOptimal2(int arr[]) {
+    int prod1 = arr[0], prod2 = arr[0], result = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      int temp = Math.max(arr[i], Math.max(prod1 * arr[i], prod2 * arr[i]));
+      prod2 = Math.min(arr[i], Math.min(prod1 * arr[i], prod2 * arr[i]));
+      prod1 = temp;
+      result = Math.max(result, prod1);
+    }
+    return result;
+  }
+
   public static void main(String[] args) {
 		int nums[] = {1, 2, -3, 0, -4, -5};
-		int answer = maxProductSubArrayBetter(nums);
+		int answer = maxProductSubArrayOptimal2(nums);
 		System.out.print("max product subarray is: "+ answer);
 	}
 }
