88f1fbd5f8702b7420ece63f621f36aca694e6d8
sanchxt <tpbeast007@gmail.com>
2024-10-17 14:47:57 +0530

    solve problem 191

---
+++ leetcode/easy/Solution191.java
@@ -0,0 +1,12 @@
+package leetcode.easy;
+
+public class Solution191 {
+  public int hammingWeight(int n) {
+    int count = 0;
+    while (n != 0) {
+      n &= n - 1;
+      count++;
+    }
+    return count;
+  }
+}
