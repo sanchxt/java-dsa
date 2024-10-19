d0c043b5103081cc49ce926ce7b1c8de57fad074
sanchxt <tpbeast007@gmail.com>
2024-10-20 03:52:32 +0530

    solve problem 49

---
+++ leetcode/medium/Solution49.java
@@ -0,0 +1,22 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution49 {
+  public List<List<String>> groupAnagrams(String[] strs) {
+    HashMap<String, List<String>> map = new HashMap<>();
+    for (String word : strs) {
+      char[] chars = word.toCharArray();
+      Arrays.sort(chars);
+
+      String sortedWord = new String(chars);
+      if (!map.containsKey(sortedWord))
+        map.put(sortedWord, new ArrayList<>());
+      map.get(sortedWord).add(word);
+    }
+    return new ArrayList<>(map.values());
+  }
+}
